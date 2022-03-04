de0bf76b3ed3244a9d61d32bfb5fe9e389bbf8a7
akevinlee <kevin.lee@microfocus.com>
2022-03-04 14:47:58 +0000

    Initial Import

---
+++ bin/fortify-sca.ps1
@@ -0,0 +1,38 @@
+#
+# Example script to perform Fortify SCA static analysis
+#
+
+# Import some supporting functions
+Import-Module $PSScriptRoot\modules\FortifyFunctions.psm1
+
+# Import local environment specific settings
+$EnvSettings = $(ConvertFrom-StringData -StringData (Get-Content ".\.env" | Where-Object {-not ($_.StartsWith('#'))} | Out-String))
+$AppName = $EnvSettings['SSC_APP_NAME']
+$AppVersion = $EnvSettings['SSC_APP_VER_NAME']
+$SSCUrl = $EnvSettings['SSC_URL']
+$SSCAuthToken = $EnvSettings['SSC_AUTH_TOKEN'] # CIToken
+$ScanSwitches = "-Dcom.fortify.sca.Phase0HigherOrder.Languages=javascript,typescript -Dcom.fortify.sca.EnableDOMModeling=true -Dcom.fortify.sca.follow.imports=true -Dcom.fortify.sca.exclude.unimported.node.modules=true"
+
+# Test we have Fortify installed successfully
+Test-Environment
+if ([string]::IsNullOrEmpty($AppName)) { throw "Application Name has not been set" }
+
+# Run the translation and scan
+
+Write-Host Running translation...
+& sourceanalyzer '-Dcom.fortify.sca.ProjectRoot=.fortify' $ScanSwitches -b "$AppName" -verbose `
+-exclude ".\src\assets\js\lib" -exclude ".\src\assets\css\lib" -exclude ".\node_modules" ".\src"
+
+Write-Host Running scan...
+& sourceanalyzer '-Dcom.fortify.sca.ProjectRoot=.fortify' $ScanSwitches -b "$AppName" -verbose `
+   -build-project "$AppName" -build-version "$AppVersion" -build-label "SNAPSHOT" -scan -f "$($AppName).fpr"
+
+Write-Host Generating PDF report...
+& ReportGenerator '-Dcom.fortify.sca.ProjectRoot=.fortify' -user "Demo User" -format pdf -f "$($AppName).pdf" -source "$($AppName).fpr"
+
+if (![string]::IsNullOrEmpty($SSCUrl)) {
+    Write-Host Uploading results to SSC...
+    & fortifyclient uploadFPR -file "$($AppName).fpr" -url $SSCUrl -authtoken $SSCAuthToken -application $AppName -applicationVersion $AppVersion
+}
+
+Write-Host Done.
